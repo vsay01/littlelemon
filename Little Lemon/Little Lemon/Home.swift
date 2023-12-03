90bd3c7f6710ebe5052089491391513452a491bc
Vortana Say <sayvortana.itc@gmail.com>
2023-12-03 12:35:14 -0600

    navigation from onboarding to home

---
+++ Little Lemon/Little Lemon/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Little Lemon
+//
+//  Created by Vortana Say on 12/3/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ Little Lemon/Little Lemon/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by Vortana Say on 12/3/23.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
